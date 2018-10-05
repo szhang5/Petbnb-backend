f62ac7d2a271d0aad382ef629f0085eff833a72a
Shiyun Zhang <shiyun.zhang.zoey@gmail.com>
2018-10-05 18:26:24 -0400

    add integration test

--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ all:
 local:
 	$(MAKE) bridge
 	$(MAKE) build
-	$(MAKE) run
+	$(MAKE) run cmd="npm start"
 
 build:
 	docker build --rm \
@@ -30,13 +30,17 @@ run:
 		--name $(SVC_NAME) \
 		-p $(GRPC_PORT):$(GRPC_PORT) \
 		-v $(PWD)/server:/usr/src/app/server \
-		$(SVC_NAME):$(SVC_TAG)
+		$(SVC_NAME):$(SVC_TAG) \
+		$(cmd)
 
 exec:
 	docker exec \
 		-ti \
 		$(SVC_NAME) \
 		/bin/sh
+
+integration:
+	$(MAKE) run cmd="/bin/sh -c 'yarn start & sleep 5 && yarn run integration-test'"
 
 stop:
 	docker stop $(SVC_NAME) || true && docker rm $(SVC_NAME) || true
--- server/test_integration/petbnb-client.js
+++ server/test_integration/petbnb-client.js
@@ -12,12 +12,11 @@ var packageDefinition = protoLoader.loadSync(
     });
 var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
 // The protoDescriptor object has the full package hierarchy
-var petBnbService = protoDescriptor.petbnbservice;
+var petBnbProto = protoDescriptor.petbnbservice;
 
 function connect() {
   return Promise.resolve(
-    new petBnbService.PetBnbService('localhost:50051', grpc.credentials.createInsecure());
-    )
+    new petBnbProto.PetBnbService('localhost:50051', grpc.credentials.createInsecure()));
 }
 
 module.exports = {
