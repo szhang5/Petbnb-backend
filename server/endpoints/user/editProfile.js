772665e3a7fa98344d2a30f85626757803995263
shiyun zhang <shiyunzhang@shiyuns-MacBook-Pro.local>
2018-11-28 09:29:16 -0500

    add editProfile endpoint and modified getUserInfo endpoint

---
+++ server/endpoints/user/editProfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const knex = require('../../models/knex');
+
+
+function insertUserInfo(firstname, lastname, email, phone, street, city, state, zip) {
+	const rawInsertQuery = `
+		UPDATE usertable
+		SET firstname = ?,
+		    lastname = ?,
+		    phone = ?,
+		    street = ?,
+		    city = ?,
+		    state = ?,
+		    zip = ?
+		WHERE
+		 username = ?;
+	`;
+	return knex.raw(rawInsertQuery, [firstname, lastname, phone, street, city, state, zip, email]);
+}
+
+function editProfile(call, callback) {
+	console.log(call);
+	return insertUserInfo(call.request.firstname, call.request.lastname, call.request.email, call.request.phone, call.request.street, call.request.city, call.request.state, call.request.zip).then(() => {
+		return callback(null, {
+			success: true,
+			user: {
+				userid: call.request.uid,
+      			personid: call.request.personid,
+				email: call.request.email,
+				firstname: call.request.firstname,
+				lastname: call.request.lastname,
+				phone: call.request.phone,
+				street: call.request.street,
+				city: call.request.city,
+				state: call.request.state,
+				zip: call.request.zip,
+			}
+		});
+	}, (err) => {
+		callback(err, null);
+	}).catch((err) => {
+		callback(err, null);
+	})
+}
+
+module.exports = {
+	editProfile,
+};
