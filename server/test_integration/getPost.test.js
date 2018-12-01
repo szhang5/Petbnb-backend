8cdb883c46c8326b39875ba54a53ea50a6146963
Shiyun Zhang <shiyun.zhang.zoey@gmail.com>
2018-12-01 17:19:03 -0500

    add getUserPost Endpoint

--- server/test_integration/getPost.test.js
+++ server/test_integration/getPost.test.js
@@ -7,7 +7,7 @@ const service = require('./petbnb-client');
 describe('test getpost endpoints', () => {
   it('gets right response message', (done) => {
     service.connect().then(
-      client => client.getPost({uid: 2}, (err, response) => {
+      client => client.getPost({uid:13}, (err, response) => {
       	console.log(response); //read the respong directly if you want;
         expect(err).to.be.equal(null);
         expect(response.success).to.equal(true);
@@ -16,9 +16,9 @@ describe('test getpost endpoints', () => {
     		expect(response.post[0]).to.have.property('avai_end_date');
     		expect(response.post[0]).to.have.property('description');
     		expect(response.post[0]).to.have.property('pet_type');
-        expect(response.post[0]).to.have.property('hour_rate');
-        expect(response.post[0]).to.have.property('pets_num');
-        done();
+	        expect(response.post[0]).to.have.property('hour_rate');
+	        expect(response.post[0]).to.have.property('pets_num');
+	        done();
       }));
   });
 });
