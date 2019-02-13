b9f43bc5f0945713586fdd9725a211c9eee39364
Shiyun Zhang <shiyun.zhang.zoey@gmail.com>
2019-02-12 19:38:25 -0500

    modified imageupload endpoint

--- server/endpoints/imageUpload.js
+++ server/endpoints/imageUpload.js
@@ -1,6 +1,9 @@
 'use strict';
+
+const knex = require('../models/knex');
 const cloudinary = require('cloudinary').v2;
 const Config = require('../config');
+
 
 cloudinary.config({ 
   cloud_name: `${Config.development.cloudinary.cloud_name}`, 
@@ -13,26 +16,39 @@ const eager_options = {
 };
 
 
+
+function storeImageURL(email, imageURL){
+	const rawQuery = `
+	UPDATE usertable
+		SET image = ?
+		WHERE
+		 username = ?;
+	`;
+	return knex.raw(rawQuery, [imageURL, email]);
+}
+
+
 function uploadImage(imageInfo){
-	cloudinary.v2.uploader.upload(imageInfo, 
+	cloudinary.uploader.upload(imageInfo, 
   	function(error, result) {
   		console.log(result, error); 
-  		const imageURL = cloudinary.url(result.public_id, eager_options);
-  		console.log(imageURL);
+  		return result.url;
   	});
 }
 
 function imageUpload(call, callback) {
-	console.log(call);
-	return uploadImage(call.request.image_base_64).then(() => {
-		return callback(null, {
-			success: true,
-		});
-	}, (err) => {
-		callback(err, null);
-	}).catch((err) => {
-		callback(err, null);
-	})
+	// console.log(call);
+	uploadImage(call.request.image_base_64).then((image)=>{
+		return storeImageURL(call.request.email, image).then(() => {
+			return callback(null, {
+				success: true,
+			});
+		}, (err) => {
+			callback(err, null);
+		}).catch((err) => {
+			callback(err, null);
+		})
+	});
 }
 
 module.exports = {
