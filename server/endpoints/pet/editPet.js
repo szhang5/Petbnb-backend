7d67e03df847b05e03f95512d337828b08dc6c86
Shiyun Zhang <shiyun.zhang.zoey@gmail.com>
2019-02-21 13:42:11 -0500

    added getPetInfoByID endpoint

--- server/endpoints/pet/editPet.js
+++ server/endpoints/pet/editPet.js
@@ -5,34 +5,30 @@ const knex = require('../../models/knex');
 
 function insertPetInfo(petid, birth, furcolor, type, petname, weight, breed) {
   const rawInsertQuery = `
-    UPDATE usertable
+    UPDATE pet
     SET birth = ?,
         furcolor = ?,
         type = ?,
         petname = ?,
-        city = ?,
         weight = ?,
-        breed = ?,
+        breed = ?
     WHERE
        petid = ?;
   `;
-  return knex.raw(rawInsertQuery, [birth, furcolor, type, petname, city, weight, breed, petid]);
+  return knex.raw(rawInsertQuery, [birth, furcolor, type, petname, weight, breed, petid]);
 }
 
 function editPet(call, callback) {
-	// console.log(call);
-	return insertPetInfo( call.request.petid , call.request.birth, call.request.furcolor, call.request.type, call.request.petname, call.request.weight, call.request.breed, call.request.image).then(() => {
+	console.log(call.request);
+	return insertPetInfo( call.request.petid , call.request.birth, call.request.furcolor, call.request.type, call.request.petname, call.request.weight, call.request.breed).then(() => {
 		return callback(null, {
-			pet: {
-				petid: call.request.petid,
-				uid: call.request.uid,
-				birth: call.request.birth,
-				furcolor: call.request.furcolor,
-				type: call.request.type,
-				petname: call.request.petname,
-				weight: call.request.weight,
-				breed: call.request.breed,
-			}
+			petid: call.request.petid,
+			birth: call.request.birth,
+			furcolor: call.request.furcolor,
+			type: call.request.type,
+			petname: call.request.petname,
+			weight: call.request.weight,
+			breed: call.request.breed,
 		});
 	}, (err) => {
 		callback(err, null);
