8a713f92d709c97575351c54ce325f0ff9c5da39
doctorpa <1753170315@qq.com>
2019-04-11 09:13:06 +0800

    allow user to select start & end date when creating transaction

--- server/test_integration/createTransaction.test.js
+++ server/test_integration/createTransaction.test.js
@@ -6,7 +6,7 @@ const service = require('./petbnb-client');
 describe('test createTransaction endpoints', () => {
   xit('gets right response message', (done) => {
     service.connect().then(
-      client => client.createTransaction({petid: [86,46,87], sitterid:53 }, (err, response) => {
+      client => client.createTransaction({petid: [86,46,87], sitterid:53, avai_start_date:"2019-05-09 18:30:00", avai_end_date:"2019-06-01 18:30:00"}, (err, response) => {
       	console.log(response); //read the respong directly if you want;
         expect(err).to.be.equal(null);
         expect(response.success).to.equal(true);
