a6fe7393c386b0c3f567c9ca2ad33b5dd61d572c
doctorpa <1753170315@qq.com>
2019-04-11 23:20:02 +0800

    PayTransaction will update status of transaction to 2

--- server/test_integration/payTransaction.test.js
+++ server/test_integration/payTransaction.test.js
@@ -5,7 +5,7 @@ const service = require('./petbnb-client');
 
 
 describe('test payTransaction endpoints', () => {
-  xit('gets right response message', (done) => {
+  it('gets right response message', (done) => {
     service.connect().then(
       client => client.payTransaction({transacid:105}, (err, response) => {
         done();
